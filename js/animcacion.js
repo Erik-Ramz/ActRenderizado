11b5878553cf1dfdcc818f897bf257ac3c63ed58
Erik Ramirez <danielrmzmares@gmail.com>
2021-11-11 11:28:18 -0600

    si en otra act

---
+++ js/animcacion.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el:'#cont',
+    data:{
+        mostrar:true,
+        mensaje:
+        {
+            transition:'Hola',
+            animacion:'Adios',
+            animacionCustom:'OwO',
+        },
+    }
+})
